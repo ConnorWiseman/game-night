c3fddd117c07513ea1912cfcec9f2e3285cbe5bc
Connor Wiseman <wiseman.connor@gmail.com>
2018-10-10 10:39:02 -0600

    don't call random, just pass it in

--- src/shared/shuffle.js
+++ src/shared/shuffle.js
@@ -10,10 +10,10 @@ const random = require('../random');
  * specified random function {rand} to generate random values for shuffle
  * comparison.
  * @param  {Array.<*>} arr
- * @param  {Function}  [rand=random()]
+ * @param  {Function}  [rand=random]
  * @return {Array.<*>}
  * @public
  */
-module.exports = (arr, rand = random()) => arr.map((each) => [ rand(), each ])
+module.exports = (arr, rand = random) => arr.map((each) => [ rand(), each ])
   .sort((a, b) => a[0] - b[0])
   .map(([ , each ]) => each);
